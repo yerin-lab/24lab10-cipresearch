963d4c2293d707f607050cc340fdc79a4f81fd8e
yerin-lab <181642401+yerin-lab@users.noreply.github.com>
2024-11-10 23:56:32 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,7 +1,9 @@
 import sys
+
 number = int(sys.argv[1])
 
-for i in range(1, number+1):
-  if number % i == 0:
-    print(i, end=" ")
+for i in range(1, number + 1):
+    if number % i == 0:
+        print(i, end=" ")
 
+print()
